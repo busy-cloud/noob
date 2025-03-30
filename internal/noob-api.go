82a48c49f811ee23cf9c284a76daa963fc002e84
jason <jason@zgwit.com>
2025-03-31 03:38:23 +0800

    添加了同步接口

--- internal/noob-api.go
+++ internal/noob-api.go
@@ -3,6 +3,7 @@ package internal
 import (
 	"github.com/busy-cloud/boat/api"
 	"github.com/busy-cloud/boat/curd"
+	"github.com/busy-cloud/boat/db"
 	"github.com/gin-gonic/gin"
 )
 
@@ -16,7 +17,54 @@ func init() {
 	api.Register("GET", "noob/noob/:id/enable", curd.ApiDisable[Noob](true))
 	api.Register("GET", "noob/noob/:id/disable", curd.ApiDisable[Noob](false))
 	api.Register("GET", "noob/noob/:id/sync", func(ctx *gin.Context) {
+		id := ctx.Param("id")
 
+		var devices []*Device = make([]*Device, 0)
+		err := db.Engine().Where("noob_id=?", id).Find(&devices)
+		if err != nil {
+			api.Error(ctx, err)
+			return
+		}
+
+		//下发指令
+		cmd, err := Request(id, Command{
+			"cmd":  "config_write",
+			"name": "devices",
+			"data": devices,
+		})
+		if err != nil {
+			api.Error(ctx, err)
+			return
+		}
+		if cmd["ret"] != 1 {
+			api.Fail(ctx, cmd["msg"].(string))
+			return
+		}
+
+		//检查连接
+		var links []*NoobLink = make([]*NoobLink, 0)
+		err = db.Engine().Where("noob_id=?", id).Find(&links)
+		if err != nil {
+			api.Error(ctx, err)
+			return
+		}
+
+		//下发指令
+		cmd, err = Request(id, Command{
+			"cmd":  "config_write",
+			"name": "links",
+			"data": links,
+		})
+		if err != nil {
+			api.Error(ctx, err)
+			return
+		}
+		if cmd["ret"] != 1 {
+			api.Fail(ctx, cmd["msg"].(string))
+			return
+		}
+
+		api.OK(ctx, nil)
 	})
 
 }
